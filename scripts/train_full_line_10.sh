d4aa9bd3440f9a5da34c56cdb60c6d5e78e6a047
Wei Zhao <zhaowei0524@outlook.com>
2021-06-06 21:42:10 -0400

    change for my PC

--- scripts/train_full_line_10.sh
+++ scripts/train_full_line_10.sh
@@ -1,4 +1,4 @@
 ############## To train the model with new line detectors #############
 python train.py --name full_line_10 --dataroot ./dataset/train/ --label_nc 0 --gpu_ids 0 --batchSize 16 --loadSize 256 --fineSize 256 --no_instance --lambda_prior 10 --continue_train --detector_type line --tf_log
 
-python train.py --name full_line_10 --dataroot ./dataset/train/ --label_nc 0 --gpu_ids 0 --batchSize 16 --loadSize 256 --fineSize 256 --no_instance --lambda_prior 10 --continue_train --detector_type line --tf_log --which_epoch best
+python train.py --name full_line_10 --dataroot ./dataset/train/ --label_nc 0 --gpu_ids 0 --batchSize 1 --loadSize 256 --fineSize 256 --no_instance --lambda_prior 10 --continue_train --detector_type line --tf_log --which_epoch best
